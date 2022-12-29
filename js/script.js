1d5c51636e5f0af4b237558afb9eb9c28628a8ef
Witold Walis <witoldwalis@gmail.com>
2022-12-29 15:39:57 +0100

    functions added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
-console.log("Czołem");
+{
 
-let button = document.querySelector(".main__button");
-let body = document.querySelector("body");
-let theme = document.querySelector(".main__theme--js");
-
-button.addEventListener("click", () => {
-    body.classList.toggle("body__whiteBackground--js");
-
-    if (body.classList.contains("body__whiteBackground--js")) {
-        theme.innerText = "szare";
-    } else {
-        theme.innerText = "białe";
+    function welcome(){
+        console.log("Czołem");
     }
-});
+
+    welcome()
+
+    function change_background() {
+        const button = document.querySelector(".main__button");
+        const body = document.querySelector("body");
+        const theme = document.querySelector(".main__theme--js");
+
+        button.addEventListener("click", () => {
+            body.classList.toggle("body__whiteBackground--js");
+
+            if (body.classList.contains("body__whiteBackground--js")) {
+                theme.innerText = "szare";
+            } else {
+                theme.innerText = "białe";
+            }
+        });
+    }
+}
